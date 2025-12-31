6200f008dd870dc9ef6ed78b702813e4bb01015a
DLeonAibar <25.on.davidleon@colegiomontessori.com>
2025-12-31 21:42:22 +0100

    Proyecto2

---
+++ Proyecto2/src/sumando/Sumando.java
@@ -0,0 +1,17 @@
+package sumando;
+
+import java.util.Scanner;
+
+public class Sumando {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scaner=new Scanner(System.in);
+		System.out.println("Introduzca numero 1: ");
+		int num1=scaner.nextInt();
+		System.out.println("Introduzca numero 2:");
+		int num2=scaner.nextInt();
+		System.out.println(num1+" "+num2);
+	}
+
+}
